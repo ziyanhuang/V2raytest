7820b68bfc7303a9656b2f80059b51b2af7eed2b
ziyanhuang <huangziyan626@gmail.com>
2022-05-29 18:08:23 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "zphv2ray",
                         "alterId": 0
                     }
                 ],
